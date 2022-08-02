8f70e0779bf6a7b166bb45625fc7b79eef0fa781
tahmimhridoy <thamimhasanhridoyea@gmail.com>
2022-08-03 00:15:16 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0fcfec",
+          secondary: "#19d3ae",
+          accent: "#3a4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
